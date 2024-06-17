cb1391697ce2757072423530ebee8ce0e5b61e69
Chetan Meshram <meshramcm684@gmail.com>
2024-06-18 00:25:22 +0530

    Initial commit

---
+++ myapp.py
@@ -0,0 +1,24 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Simple Stock Price App
+
+Shown are the stock closing price and volume of Google!
+""")
+
+# Define the ticker symbol
+tickerSymbol = 'GOOGL'
+
+# Get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+
+# Get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2019-5-31', end='2024-5-31')
+
+# Display the closing price
+st.line_chart(tickerDf.Close)
+
+# Display the volume
+st.line_chart(tickerDf.Volume)
